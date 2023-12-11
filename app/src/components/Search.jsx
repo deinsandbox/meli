9e4d156886b22e974a668b1f6f492c4464fb4fc9
equiman <equiman@users.noreply.github.com>
2023-12-10 23:42:06 -0500

    feat(app): :sparkles: search component

--- app/src/components/Search.jsx
+++ app/src/components/Search.jsx
@@ -1,4 +1,23 @@
+import './Search.scss'
+import IconSearch from '../assets/icon-search.svg'
+
 const Search = () => {
-  return <div>Search</div>
+  return (
+    <>
+      <form className="search-form" action="/items" method="get">
+        <input
+          type="search"
+          id="search"
+          name="search"
+          className="search-input"
+          placeholder="Nunca dejes de buscar"
+          autoFocus
+        />
+        <button type="submit" className="search-button">
+          <img src={IconSearch} alt="Buscar" className="search-button-icon" />
+        </button>
+      </form>
+    </>
+  )
 }
 export default Search
